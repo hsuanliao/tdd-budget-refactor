ca9344fa1bc3b3e55d4ee77ad2cd5e3e6580798d
Joey Chen <jay.hatelove@gmail.com>
2019-07-27 22:24:46 +0800

    inline variables: firstDay and lastDay

--- BudgetApp/BudgetApp/Period.cs
+++ BudgetApp/BudgetApp/Period.cs
@@ -21,21 +21,19 @@ namespace BudgetApp
         public int OverlappingDayCount(Budget budget)
         {
             var another = new Period(budget.FirstDay(), budget.LastDay());
-            var firstDay = another.StartDate;
-            var lastDay = another.EndDate;
 
-            if (EndDate < firstDay || StartDate > lastDay)
+            if (EndDate < another.StartDate || StartDate > another.EndDate)
             {
                 return 0;
             }
 
-            var overlappingStart = StartDate > firstDay
+            var overlappingStart = StartDate > another.StartDate
                 ? StartDate
-                : firstDay;
+                : another.StartDate;
 
-            var overlappingEnd = EndDate < lastDay
+            var overlappingEnd = EndDate < another.EndDate
                 ? EndDate
-                : lastDay;
+                : another.EndDate;
 
             return DayCount(overlappingStart, overlappingEnd);
         }
